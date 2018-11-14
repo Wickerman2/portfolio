2bd8dcdaeecb0882e13721f9377920e94fd0825c
Christian Wickerström <christianwickerstrom@Christians-MacBook-Pro.local>
2018-11-14 13:15:22 +0100

    adding bootstrap

--- wp-content/themes/portfolio/functions.php
+++ wp-content/themes/portfolio/functions.php
@@ -1,10 +1,17 @@
 <?php
 
-function enqueue_styles() {
-    wp_enqueue_style( "stylesheet", get_template_directory_uri() . "/style.css");
-    wp_enqueue_style( "bs_css", "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css");
-}
-add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
+function themebs_enqueue_styles() {
+
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
+    wp_enqueue_style( 'core', get_template_directory_uri() . '/style.css' );
+  
+  }
+  add_action( 'wp_enqueue_scripts', 'themebs_enqueue_styles');
+  
+  function themebs_enqueue_scripts() {
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.bundle.min.js', array( 'jquery' ) );
+  }
+  add_action( 'wp_enqueue_scripts', 'themebs_enqueue_scripts');
 
 if ( ! function_exists ( 'portfolio_setup' ) ) :
     function portfolio_setup() {
@@ -26,3 +33,13 @@ function remove_admin_login_header() {
     remove_action('wp_head', '_admin_bar_bump_cb');
 }
 add_action('get_header', 'remove_admin_login_header');
+
+require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+
+if ( ! file_exists( get_template_directory() . '/class-wp-bootstrap-navwalker.php' ) ) {
+	// file does not exist... return an error.
+	return new WP_Error( 'class-wp-bootstrap-navwalker-missing', __( 'It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
+} else {
+	// file exists... require it.
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -12,12 +12,33 @@
 
 <header class="header">  
 <?php 
-
+/*
 wp_nav_menu( $arg = array(
   'menu_class' => 'main-navigation',
   'theme_location' => 'primary'
 ) );
-
+*/
 ?>
+<div id="main_navbar" class="navbar navbar-expand-md navbar-light bg-light">
+    <!-- you can remove this container wrapper if you want things full width -->
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#headerNav" aria-controls="headerNav" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'best-reloaded' ); ?>">
+            <span class="navbar-toggler-icon"></span><span class="sr-only"><?php esc_html_e( 'Toggle Navigation', 'themeslug' ); ?></span>
+        </button>
+        <nav class="collapse navbar-collapse" id="headerNav" role="navigation" aria-label="Main Menu">
+            <span class="sr-only"><?php esc_html_e( 'Main Menu', 'themeslug' ); ?></span>
+            <?php
+            wp_nav_menu( array(
+                'theme_location' => 'primary',
+                'depth' => 1,
+                'container' => false,
+                'menu_class' => 'main-navigation',
+                'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                'walker' => new WP_Bootstrap_Navwalker(),
+            ) );
+        ?>
+        </nav>
+    </div>
+</div>
 
 </header>
